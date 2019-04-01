4da0c5643a4d5eb20b424e356d1543784fe4eb53
MaddulaSaiKrishna <m.saikrishna1992@gmail.com>
2019-04-01 22:00:43 +0530

    Updated index.ts
    Updated index.ts with provision to add 3 students and 3 teachers in database if the records are empty.

--- node-assessment/src/index.ts
+++ node-assessment/src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import {createConnection, getRepository} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import {Request, Response} from "express";
@@ -39,33 +39,33 @@ createConnection().then(async connection => {
         });
     });
 
-    // setup express app here
-    // ...
+    // insert new users for testing purposes.
+    const teacherRepo = getRepository(Teacher);
+    const allTeachers = await teacherRepo.find();
+    if (allTeachers.length === 0 ){
+        await teacherRepo.save([{
+            email: "teacher1@gmail.com"
+        }, {
+            email: "teacher2@gmail.com"
+        }, {
+            email: "teacher3@gmail.com"
+        }]);
+    }
+
+    const studentRepo = getRepository(Student);
+    const allStudents = await studentRepo.find();
+    if (allStudents.length === 0) {
+        await studentRepo.save([{
+            email: "student1@gmail.com"
+        }, {
+            email: "student2@gmail.com"
+        }, {
+            email: "student3@gmail.com"
+        }]);
+    }
 
     // start express server
     app.listen(3000);
-
-    // insert new users for test
-    // await connection.manager.save(connection.manager.create(Teacher, {
-    //     email: "teacher1@gmail.com"
-    // }));
-    // await connection.manager.save(connection.manager.create(Teacher, {
-    //     email: "teacher2@gmail.com"
-    // }));
-    // await connection.manager.save(connection.manager.create(Teacher, {
-    //     email: "teacher3@gmail.com"
-    // }));
-
-    // await connection.manager.save(connection.manager.create(Student, {
-    //     email: "student1@gmail.com"
-    // }));
-    // await connection.manager.save(connection.manager.create(Student, {
-    //     email: "student2@gmail.com"
-    // }));
-    // await connection.manager.save(connection.manager.create(Student, {
-    //     email: "student3@gmail.com"
-    // }));
-
     console.log("Express server has started on port 3000.");
 
 }).catch(error => console.log(error));
